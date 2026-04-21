368c5041568f023be639277250e9a91dd9a60e5e
genaton <genaton@gmail>
2026-04-21 06:55:04 -0300

    para efeitos didáticos cria-se uma nova classe principal já refatorada

--- src/main/java/br/com/alura/AdopetConsoleApplication_V3.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication_V3.java
@@ -17,7 +17,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class AdopetConsoleApplication {
+public class AdopetConsoleApplication_V3 {
 
     public static void main(String[] args) {
 
